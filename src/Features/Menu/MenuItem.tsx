6306397a468f077f48a682b7bb3420cdf2bb4142
jawad-mwadama <allymwadama@gmail.com>
2025-12-02 21:51:36 +0300

    fixed the responsive issue

--- src/Features/Menu/MenuItem.tsx
+++ src/Features/Menu/MenuItem.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+/* React import not required with the automatic JSX runtime */
 
 interface MenuItemProps {
   dessertData: {
@@ -18,29 +18,27 @@ function MenuItem({ dessertData }: MenuItemProps) {
   const { thumbnail, mobile, tablet, desktop } = dessertData.image;
 
   return (
-    <div className="flex font-redHat">
-      <div className="w-1/3 p-4">
-        <picture>
-          {/* desktop */}
-          <source media="(min-width:1024px)" srcSet={desktop} />
-          {/* tablet */}
-          <source media="(min-width: 768px)" srcSet={tablet} />
-          {/* mobile */}
-          <source media="(min-width: 480px)" srcSet={mobile} />
-          {/* fallback */}
-          <img
-            src={thumbnail}
-            alt={dessertData.name}
-            className="h-42 rounded-lg hover:border-2 hover:border-red"
-          />
-        </picture>
+    <div className="flex flex-col gap-2 p-2 font-redHat">
+      <picture>
+        {/* desktop */}
+        <source media="(min-width:1024px)" srcSet={desktop} />
+        {/* tablet */}
+        <source media="(min-width: 768px)" srcSet={tablet} />
+        {/* mobile */}
+        <source media="(min-width: 480px)" srcSet={mobile} />
+        {/* fallback */}
+        <img
+          src={thumbnail}
+          alt={dessertData.name}
+          className="h-auto w-full rounded-lg hover:border-2 hover:border-red"
+        />
+      </picture>
 
-        <p className="pt-1.5 text-sm text-stone-400">{dessertData.category}</p>
-        <h2 className="f font-semibold text-rose-900">{dessertData.name}</h2>
-        <span className="text-xl font-bold text-red">
-          ${dessertData.price.toFixed(2)}
-        </span>
-      </div>
+      <p className="text-sm text-stone-400">{dessertData.category}</p>
+      <h2 className="font-semibold text-rose-900">{dessertData.name}</h2>
+      <span className="text-xl font-bold text-red">
+        ${dessertData.price.toFixed(2)}
+      </span>
     </div>
   );
 }

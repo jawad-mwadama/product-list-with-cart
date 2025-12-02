d32a4d19915e07610ca5f55878afbfe7bfce7f8a
jawad-mwadama <allymwadama@gmail.com>
2025-12-02 21:27:51 +0300

    optimized images for respective breakpoints

--- src/Features/Menu/MenuItem.tsx
+++ src/Features/Menu/MenuItem.tsx
@@ -15,14 +15,26 @@ interface MenuItemProps {
 }
 
 function MenuItem({ dessertData }: MenuItemProps) {
+  const { thumbnail, mobile, tablet, desktop } = dessertData.image;
+
   return (
     <div className="flex font-redHat">
       <div className="w-1/3 p-4">
-        <img
-          src={dessertData.image.thumbnail}
-          alt={dessertData.name}
-          className="h-42 rounded-lg hover:border-2 hover:border-red"
-        />
+        <picture>
+          {/* desktop */}
+          <source media="(min-width:1024px)" srcSet={desktop} />
+          {/* tablet */}
+          <source media="(min-width: 768px)" srcSet={tablet} />
+          {/* mobile */}
+          <source media="(min-width: 480px)" srcSet={mobile} />
+          {/* fallback */}
+          <img
+            src={thumbnail}
+            alt={dessertData.name}
+            className="h-42 rounded-lg hover:border-2 hover:border-red"
+          />
+        </picture>
+
         <p className="pt-1.5 text-sm text-stone-400">{dessertData.category}</p>
         <h2 className="f font-semibold text-rose-900">{dessertData.name}</h2>
         <span className="text-xl font-bold text-red">

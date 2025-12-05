af5b3523caf68952623881f73a6d85738d9e85b0
jawad-mwadama <allymwadama@gmail.com>
2025-12-05 23:33:20 +0300

    started working on the empty cart

--- src/Features/Cart/Cart.tsx
+++ src/Features/Cart/Cart.tsx
@@ -1,30 +1,39 @@
 function Cart() {
   return (
-    <div className="rounded-lg bg-rose-50 py-3">
-      <h2 className="px-3.5 py-1 text-xl font-bold text-red">Your Cart (7)</h2>
+    <>
+      <div className="rounded-lg bg-rose-50 py-3">
+        <h2 className="px-3.5 py-1 text-xl font-bold text-red">
+          Your Cart (7)
+        </h2>
 
-      {/* divider for all items except the last */}
-      <div className="divide-y divide-gray-200">
-        <div className="flex items-center justify-between px-3.5 py-2">
-          {/* left */}
-          <div className="flex flex-col">
-            <span className="py-1 font-semibold text-rose-900">
-              Classic Tiramisu
-            </span>
+        {/* divider for all items except the last */}
+        <div className="divide-y divide-gray-200">
+          <div className="flex items-center justify-between px-3.5 py-2">
+            {/* left */}
+            <div className="flex flex-col">
+              <span className="py-1 font-semibold text-rose-900">
+                Classic Tiramisu
+              </span>
 
-            <div className="flex items-center gap-2 text-sm text-gray-600">
-              <span className="font-semibold text-rose-600">1x</span>
-              <span>@ $5.50</span>
-              <span className="font-semibold text-gray-900">$5.50</span>
+              <div className="flex items-center gap-2 text-sm text-gray-600">
+                <span className="font-semibold text-rose-600">1x</span>
+                <span>@ $5.50</span>
+                <span className="font-semibold text-gray-900">$5.50</span>
+              </div>
             </div>
-          </div>
 
-          {/* right */}
-          <button className="text-gray-400 hover:text-gray-600">✕</button>
+            {/* right */}
+            <button className="text-gray-400 hover:text-gray-600">✕</button>
+          </div>
         </div>
       </div>
-    </div>
+
+      <EmptyCart />
+    </>
   );
 }
 
+function EmptyCart() {
+  return <div></div>;
+}
 export default Cart;

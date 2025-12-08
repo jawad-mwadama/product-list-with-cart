7a2bfe924c10c781f2db0deed0ea5d08dafece82
jawad-mwadama <allymwadama@gmail.com>
2025-12-08 22:56:24 +0300

    refactored ui

--- src/Features/Order/OrderConfirmedModal.tsx
+++ src/Features/Order/OrderConfirmedModal.tsx
@@ -9,11 +9,11 @@ interface Modal {
 function OrderConfirmedModal({ open, onClose }: Modal) {
   return (
     <div
-      className={`fixed inset-0 z-50 mx-auto flex items-center justify-center transition-colors ${open ? 'visible bg-black/40' : 'invisible'}`}
+      className={`fixed inset-0 z-50 mx-auto flex items-center justify-center font-redHat transition-colors ${open ? 'visible bg-black/40' : 'invisible'}`}
     >
       <div
         onClick={(e) => e.stopPropagation()}
-        className={`rounded-lg bg-rose-50 p-6 transition-all ${open ? 'scale-100 opacity-100' : 'scale-125 opacity-0'}`}
+        className={`rounded-lg bg-rose-50 p-4 transition-all ${open ? 'scale-100 opacity-100' : 'scale-125 opacity-0'}`}
       >
         <button
           className="absolute top-2 right-2 rounded-full border border-red p-1.5"
@@ -22,9 +22,9 @@ function OrderConfirmedModal({ open, onClose }: Modal) {
           <img src={X} alt="remove" />
         </button>
         <span>
-          <img src={confirm} alt="order-confirmed" />
+          <img src={confirm} alt="order-confirmed" className="h-9 w-9" />
         </span>
-        <h2>Order now</h2>
+        <h2 className="pt-3 text-2xl font-bold text-rose-900">Order now</h2>
         <p>We hope you enjoy your food!</p>
       </div>
     </div>
